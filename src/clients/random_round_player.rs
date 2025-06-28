830fa5fac414c9f1a0c313e9697a03b0ea1d3df1
Home <home@Ivans-MacBook-Pro.local>
2025-06-28 13:41:05 +0200

    Refactor out console helpers

--- src/clients/random_round_player.rs
+++ src/clients/random_round_player.rs
@@ -1,47 +1,15 @@
-use std::io::{self, Write};
-
 use rand::random_range;
 
 use crate::{
     game::{
         deck::CardSuit,
-        player::Team,
         round::{Round, RoundPlayer, RoundUpdateEvent},
     },
-    utils::random::get_random_suit,
+    utils::{console::{clear_console, print_current_points, wait_for_std_input}, random::get_random_suit},
 };
 
 #[derive(Debug)]
 pub struct RandomRoundPlayer;
-
-fn wait_for_std_input() {
-    let mut buffer = String::new();
-    io::stdin().read_line(&mut buffer).unwrap();
-}
-
-fn clear_console() {
-    print!("\x1B[2J\x1B[1;1H");
-    std::io::stdout().flush().unwrap();
-}
-
-fn print_current_points(round_state: &Round) {
-    let declarations = &round_state.team_declarations;
-    let team_a_points = round_state.points.get_points(Team::A);
-    let team_b_points = round_state.points.get_points(Team::B);
-    let team_a_declarations = declarations.get_points_sum(&Team::A);
-    let team_b_declarations = declarations.get_points_sum(&Team::B);
-    let team_a_points = team_a_points + team_a_declarations;
-    let team_b_points = team_b_points + team_b_declarations;
-    let trump = &round_state.trump;
-    let trump_player = &round_state.get_player_by_index(trump.player_index).name;
-    let tump_color: &str = trump.trump_suit.clone().into();
-
-    println!(
-        "TEAM A: {}    TEAM B: {}      TRUMP: {} - ({})",
-        team_a_points, team_b_points, tump_color, trump_player
-    );
-    println!("");
-}
 
 impl RoundPlayer for RandomRoundPlayer {
     fn try_call_trump(&self, _round_state: &Round, _player_index: usize) -> Option<CardSuit> {
---
+++ src/utils/console.rs
@@ -0,0 +1,33 @@
+use std::io::{self, stdout, Write};
+
+use crate::game::{player::Team, round::Round};
+
+
+pub fn wait_for_std_input() {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+}
+
+pub fn clear_console() {
+    print!("\x1B[2J\x1B[1;1H");
+    stdout().flush().unwrap();
+}
+
+pub fn print_current_points(round_state: &Round) {
+    let declarations = &round_state.team_declarations;
+    let team_a_points = round_state.points.get_points(Team::A);
+    let team_b_points = round_state.points.get_points(Team::B);
+    let team_a_declarations = declarations.get_points_sum(&Team::A);
+    let team_b_declarations = declarations.get_points_sum(&Team::B);
+    let team_a_points = team_a_points + team_a_declarations;
+    let team_b_points = team_b_points + team_b_declarations;
+    let trump = &round_state.trump;
+    let trump_player = &round_state.get_player_by_index(trump.player_index).name;
+    let tump_color: &str = trump.trump_suit.clone().into();
+
+    println!(
+        "TEAM A: {}    TEAM B: {}      TRUMP: {} - ({})",
+        team_a_points, team_b_points, tump_color, trump_player
+    );
+    println!("");
+}
