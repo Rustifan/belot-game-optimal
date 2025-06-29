24128e849e45ee2eddae155c673291b1a039bb53
Home <home@Ivans-MacBook-Pro.local>
2025-06-29 14:04:30 +0200

    Refactor TeamPoints

--- src/game/round.rs
+++ src/game/round.rs
@@ -1,14 +1,11 @@
-use strum::{EnumCount, IntoEnumIterator};
+use strum::IntoEnumIterator;
 
 use crate::game::team::Team;
 use crate::game::declaration::DeclaratonWithPlayerInfo;
 use crate::game::round_player::RoundPlayer;
 
 use super::{
-    deck::{Card, Deck},
-    declaration::{get_possible_declarations, Declaration, TeamDeclarations},
-    player::{Player, Players, NUMBER_OF_PLAYERS},
-    trick::{Trick, TrickHistoryItem}, trump::Trump 
+    deck::{Card, Deck}, declaration::{get_possible_declarations, Declaration, TeamDeclarations}, player::{Player, Players, NUMBER_OF_PLAYERS}, team::TeamPoints, trick::{Trick, TrickHistoryItem}, trump::Trump 
 };
 
 pub enum RoundUpdateEvent<'a> {
@@ -25,37 +22,6 @@ pub enum RoundUpdateEvent<'a> {
         player_index: usize,
     },
     TrickDone(TrickHistoryItem),
-}
-
-#[derive(Debug, Clone, Default)]
-pub struct TeamPoints {
-    points: [usize; Team::COUNT],
-}
-
-impl TeamPoints {
-    pub fn add_points(&mut self, team: Team, points: usize) {
-        let index = team.to_index();
-        self.points[index] += points;
-    }
-
-    pub fn has_bigger_points(&self, team: Team) -> bool {
-        let team_index = team.to_index();
-        let other_team_index = (team_index + 1) % Team::COUNT;
-
-        self.points[team_index] > self.points[other_team_index]
-    }
-
-    pub fn all_points_to_team(&mut self, team: Team) {
-        let team_index = team.to_index();
-        let other_team_index = (team_index + 1) % Team::COUNT;
-        let points_sum = self.points.iter().fold(0, |acc, curr| acc + *curr);
-        self.points[team_index] = points_sum;
-        self.points[other_team_index] = 0;
-    }
-
-    pub fn get_points(&self, team: Team) -> usize {
-        self.points[team.to_index()]
-    }
 }
 
 #[derive(Debug, Clone)]
--- src/game/team.rs
+++ src/game/team.rs
@@ -39,3 +39,34 @@ impl Team {
         Self::from_index(index)
     }
 }
+
+#[derive(Debug, Clone, Default)]
+pub struct TeamPoints {
+    points: [usize; Team::COUNT],
+}
+
+impl TeamPoints {
+    pub fn add_points(&mut self, team: Team, points: usize) {
+        let index = team.to_index();
+        self.points[index] += points;
+    }
+
+    pub fn has_bigger_points(&self, team: Team) -> bool {
+        let team_index = team.to_index();
+        let other_team_index = (team_index + 1) % Team::COUNT;
+
+        self.points[team_index] > self.points[other_team_index]
+    }
+
+    pub fn all_points_to_team(&mut self, team: Team) {
+        let team_index = team.to_index();
+        let other_team_index = (team_index + 1) % Team::COUNT;
+        let points_sum = self.points.iter().fold(0, |acc, curr| acc + *curr);
+        self.points[team_index] = points_sum;
+        self.points[other_team_index] = 0;
+    }
+
+    pub fn get_points(&self, team: Team) -> usize {
+        self.points[team.to_index()]
+    }
+}
